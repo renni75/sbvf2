fbebfd7362f5b420d4518cbcca06ee7c32383d29
Fabian Erni <ferni@snowflake.ch>
2013-05-22 15:16:17 +0200

    * Added custom template for mobile view

--- themes/swissbibmobile/theme.config.php
+++ themes/swissbibmobile/theme.config.php
@@ -4,8 +4,8 @@ return array(
     'css' => array(
 		'jquery.mobile-1.3.1.min.css',
         'jquery.mobile.swissbib.css',
-
-    ),
+		'swissbib.css'
+	),
     'js' => array(
 		'script.js'
     ),
